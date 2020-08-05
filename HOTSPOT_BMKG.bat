b47065070e5e37ae8f801cf37a7fc9ea2679844d
miko89 <rakayasi.miko@gmail.com>
2020-08-05 15:15:19 +0700

    updatehotspot

--- HOTSPOT_BMKG.bat
+++ HOTSPOT_BMKG.bat
@@ -1,4 +1,4 @@
-set day=0
+set day=-1
 echo >"%temp%\%~n0.vbs" s=DateAdd("d",%day%,now) : d=weekday(s)
 echo>>"%temp%\%~n0.vbs" WScript.Echo year(s)^& right(100+month(s),2)^& right(100+day(s),2)
 for /f %%a in ('cscript /nologo "%temp%\%~n0.vbs"') do set "result=%%a"
@@ -6,7 +6,6 @@ del "%temp%\%~n0.vbs"
 set "YYYY=%result:~0,4%"
 set "MM=%result:~4,2%"
 set "DD=%result:~6,2%"
-
 
 F:
 cd spartan_fdrs
