2c9874082838ad0d6c20863a806c78287d7d788f
miko89 <rakayasi.miko@gmail.com>
2020-08-07 10:39:02 +0700

    upate hotspot

---
+++ VIS_BMKG .bat
@@ -0,0 +1,21 @@
+set day=0
+echo >"%temp%\%~n0.vbs" s=DateAdd("d",%day%,now) : d=weekday(s)
+echo>>"%temp%\%~n0.vbs" WScript.Echo year(s)^& right(100+month(s),2)^& right(100+day(s),2)
+for /f %%a in ('cscript /nologo "%temp%\%~n0.vbs"') do set "result=%%a"
+del "%temp%\%~n0.vbs"
+set "YYYY=%result:~0,4%"
+set "MM=%result:~4,2%"
+set "DD=%result:~6,2%"
+
+F:
+cd spartan_fdrs
+cd data
+md visibility
+cd visibility
+mkdir %yyyy%%mm%%dd%
+cd %yyyy%%mm%%dd%
+mkdir pagi
+cd pagi
+wget https://www.ogimet.com/ultimos_synops2.php?lang=en&estado=Indon&fmt=txt&Send=Send
+
+pause
